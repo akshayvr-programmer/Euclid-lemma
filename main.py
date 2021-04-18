449770db838d232839830dac8090d5caf6feb1e0
Akshay V.R <64145832+akshayvr-programmer@users.noreply.github.com>
2021-04-18 19:15:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+import math
+
+def euclid(x, y):
+  
+  x = int(input("Please Enter the first number"))
+  y = int(input("Please enter the second  number"))
+  q = math.floor(x / y)
+  r = x % y
+  
+  print(String(x)  + " " + "=" + " " + String(y) + "*" + String(q) + " " + String(r))
+  
+  
+  
+euclid(256, 64)  
+  
+  
+  
